5e1af990cbf5381e6c41437d262fdb044e1ca56a
Bedrock02 <JimSteve91@gmal.com>
2014-01-04 23:06:04 -0500

    Refactored code and tested newly added Model Functions

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -6,19 +6,24 @@ describe Game do
  	end
 
  	it "should have a game name" do
- 		@game.game_name.should be_present
+ 		@game.gameName.should be_present
  	end
 
  	it "should have a score limit" do
- 		@game.score_limit.should be_present
+ 		@game.scoreLimit.should be_present
  	end
 
  	it "should have at least one player on each team" do
- 		@game.number_of_players.should be >= 1
+ 		@game.numberOfPlayers.should be >= 1
  	end
 
  	it "should not have a winner" do
- 		@game.winner_team.should be_nil
+ 		@game.winnerTeam.should be_nil
+ 	end
+
+ 	it "should set the winner to nil" do
+ 		@game.setWinnerTeam(nil)
+ 		@game.winnerTeam.should be_nil
  	end
 
  	it "should have no teams" do 
@@ -30,7 +35,68 @@ describe Game do
  		@game.teams << team 
  		@game.reload
 
- 		@game.teams.size.should be == 1
+ 		@game.numberOfTeams.should be == 1
+ 	end
+
+ 	it "should indicate player 0's turn" do 
+ 		@game.nextPlayer
+ 		@game.playerTurn.should be == 0
+ 	end
+
+ 	it "should go to the next player's turn" do 
+ 		2.times do 
+ 			@game.nextPlayer
+ 		end
+ 		@game.playerTurn.should be == 1
+ 	end
+
+ 	it "should go back to player 0 after player 1" do 
+ 		@game.numberOfPlayers.times do
+ 			@game.nextPlayer
+ 		end
+
+ 		@game.playerTurn.should be == 1
+ 	end
+
+ 	it "should indicate team 0's turn" do 
+ 		game = FactoryGirl.create(:game_with_multiple_teams)
+ 		game.nextTeam
+ 		game.teamTurn.should be == 0
+ 	end
+
+ 	
+ 	it "should go to the next team" do
+		game = FactoryGirl.create(:game_with_multiple_teams) 		
+		game.nextTeam
+
+		2.times do
+			game.nextTeam
+		end
+ 		game.teamTurn.should be == 2
+ 	end
+
+ 	it "should go back to the first time after the last team" do
+ 		game = FactoryGirl.create(:game_with_multiple_teams)
+ 		game.nextTeam
+
+ 		game.numberOfTeams.times do
+ 			game.nextTeam
+ 		end
+ 		game.teamTurn.should be == 0
+ 	end
+
+ 	it "should get the first team" do
+ 		game = FactoryGirl.create(:game_with_multiple_teams)
+ 		game.nextTeam
+ 		game.getTeam.should be == game.teams[0]
+ 	end
+
+ 	it "should ge the first player from the first team" do
+ 		game = FactoryGirl.create(:game_with_multiple_teams)
+ 		game.nextTeam
+ 		game.nextPlayer
+
+ 		game.getPlayer.should be == game.teams[0].players[0]
  	end
 
 end
