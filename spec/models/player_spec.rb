fe4540712424b799cae9d39b4c8a2640478ed3ca
Bedrock02 <JimSteve91@gmal.com>
2014-01-15 18:00:00 -0500

    Removed tests that looked for turn number

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -13,10 +13,6 @@ describe Player do
   	@player.score.should be == 0
   end
 
-  it "should not have a turn number" do
-  	@player.turn_number.should be_nil
-  end
-
   it "should have a team" do 
   	team = FactoryGirl.create(:team)
   	team.players << @player 
@@ -24,31 +20,13 @@ describe Player do
   	@player.team.should be_present
   end
 
-  it "should have a turn number" do 
-  	game = FactoryGirl.create(:game)
-  	team = FactoryGirl.create(:team)
-  	
-  	game.teams << team
-  	game.reload 
-
-  	#if we can add a player we add a player
-  	if team.players.size < game.number_of_players
-  		@player.turn_number = team.players.size 
-  	end
-
-  	team.players << @player
-  	team.reload
-
-  	@player.turn_number.should be_present
-  end
-
   it "should increment its score" do 
-    @player.addToScore(400)
+    @player.add_to_score(400)
     @player.score.should be == 400
   end
 
   it "should remove for its score" do
-    @player.reduceFromScore(100)
+    @player.reduce_from_score(100)
     @player.score.should be == -100
   end
 
